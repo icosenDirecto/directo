7bdd5b6c13092c317765cc1be1c6b3f091f8e3e1
icosenDirecto <icosentino@directo.com.ar>
2022-10-20 16:25:28 -0300

    v2.0

--- operaciones.js
+++ operaciones.js
@@ -13,7 +13,7 @@ formOperaciones.addEventListener('submit', function(e) {
         object[key] = value
     });
     var json = JSON.stringify(object);
-    result.innerHTML = "Please wait..."
+    resultOperaciones.innerHTML = "Please wait..."
 
     fetch('https://api.web3forms.com/submit', {
             method: 'POST',
@@ -26,20 +26,20 @@ formOperaciones.addEventListener('submit', function(e) {
         .then(async (response) => {
             let json = await response.json();
             if (response.status == 200) {
-                result.innerHTML = json.message;
+                resultOperaciones.innerHTML = json.message;
             } else {
                 console.log(response);
-                result.innerHTML = json.message;
+                resultOperaciones.innerHTML = json.message;
             }
         })
         .catch(error => {
             console.log(error);
-            result.innerHTML = "Something went wrong!";
+            resultOperaciones.innerHTML = "Something went wrong!";
         })
         .then(function() {
-            form.reset();
+            formOperaciones.reset();
             setTimeout(() => {
-                result.style.display = "none";
+                resultOperaciones.style.display = "none";
             }, 3000);
         });
 });
