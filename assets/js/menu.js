ba552fbe4acf2c257945ad38bd592a0564275412
icosenDirecto <111062633+icosenDirecto@users.noreply.github.com>
2022-10-21 16:44:24 -0300

    v2.0

---
+++ assets/js/menu.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function navMobile() {
+  document.getElementById("mobile-main").classList.toggle("open");
+}
